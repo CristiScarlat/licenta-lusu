78e6acd9f33186c25eac5332813234e79626ccf1
scarlat-trainer <78.cristi@gmail.com>
2025-04-26 20:58:36 +0300

    add start default browser

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ function runWebserver() {
     const server = http.createServer(requestListener);
     server.listen(port, host, () => {
         console.log(`Server is running on http://${host}:${port}`);
-        //open(`http://${host}:${port}`, { app: ['google chrome', '--kiosk'] });
+        open(`http://${host}:${port}`, { app: ['google chrome', '--kiosk'] });
     });
 }
 
